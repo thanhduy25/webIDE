8b82cbc4c6d06611c11dc1d1884311ba117f6a92
Le Trung Kien <kienle.ag@gmail.com>
2024-06-06 16:11:39 +0700

    handle delete commit

--- src/utilities/contextMenuItems/handleDelete.js
+++ src/utilities/contextMenuItems/handleDelete.js
@@ -3,6 +3,8 @@ import {
   updateTreeDirectoryFlatten,
 } from "../../store/treeSlice";
 
+import { createDeleteAction, addAction } from "../actionStorage";
+
 const handleDelete = (fileTarget, dispatch) => {
   const fileTargetPath = fileTarget.path;
 
@@ -14,6 +16,27 @@ const handleDelete = (fileTarget, dispatch) => {
   );
 
   dispatch(convertFlattenToNestedTreeDirectory());
+
+  const actionsList = localStorage.actions
+    ? JSON.parse(localStorage.actions)
+    : [];
+
+  let indexIfFileCreatedAfter = null;
+
+  actionsList.forEach((action, index) => {
+    if (action.action === "create" && action.file_path === fileTargetPath) {
+      indexIfFileCreatedAfter = index;
+      return;
+    }
+  });
+
+  if (indexIfFileCreatedAfter !== null) {
+    actionsList.splice(indexIfFileCreatedAfter, 1);
+    localStorage.actions = JSON.stringify(actionsList);
+  } else {
+    const deleteAction = createDeleteAction(fileTargetPath);
+    addAction(deleteAction);
+  }
 };
 
 export default handleDelete;
